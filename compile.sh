8d0bc223d3148b7697cec2b16c71442073f76e12
SuperDasher <manupicc02@gmail.com>
2022-09-26 01:10:52 +0200

    add compile.sh to git repo

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#if the directory src does not exist, exit the script
+if [ ! -d src ]; then
+    echo "src directory does not exist"
+    exit 1
+fi
+
+#remove every file in out if the directory exists, otherwise create an empty out directory
+if [ -d out ]; then
+    rm -rf out/*
+else
+    mkdir out
+fi
+
+#compile every .c file in src as .o extension and every .cpp file as .opp extension and put them in out
+for file in src/*; do
+    if [[ "${file##*.}" == c ]]; then
+        gcc "$file" -o "out/$(basename -- "$file" .c).o"
+    elif [[ "${file##*.}" == cpp ]]; then
+        g++ "$file" -o "out/$(basename -- "$file" .cpp).opp"
+    fi
+done
