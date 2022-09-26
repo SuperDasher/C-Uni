684dba3eaf4d22845e67e6a8823003c5af7b5a41
SuperDasher_mobile <manupicc02@gmail.com>
2022-09-26 08:34:10 +0000

    modify base_converter.c
    added a function convert() to improve readability and made the result long long int to correctly convert bigger numbers

--- src/base_converter.c
+++ src/base_converter.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void convert(int, int);
+
 int main()
 {
 	int number;
@@ -7,28 +9,27 @@ int main()
 	printf("inserisci un numero\n");
 	scanf("%d", &number);
 
-	int result_2 = 0;
-	int result_8 = 0;
-
-	int n = number;
-	int power = 1;
-	while (n > 0)
-	{
-		result_2 += power * (n % 2);
-		n /= 2;
-		power *= 10;
-	}
-	n = number;
-	power = 1;
-	while (n > 0)
-	{
-		result_8 += power * (n % 8);
-		n /= 8;
-		power *= 10;
-	}
-
-	printf("il numero in base 2 e' %d\n", result_2);
-	printf("il numero in base 8 e' %d\n", result_8);
+	convert(number, 2);
+	convert(number, 8);
 
 	return 0;
+}
+
+void convert(int number, int base)
+{
+	if (base > 10)
+	{
+		printf("non posso convertire in base %d\n", base);
+		return;
+	}
+	int n = number;
+	long long int result = 0;
+	long long int power = 1;
+	while (n > 0)
+	{
+		result += power * (n % base);
+		n /= base;
+		power *= 10;
+	}
+	printf("%d in base %d e' %lld\n", number, base, result);
 }
