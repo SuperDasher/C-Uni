c172f7cbb02023f9a4efac6f4423739d136d1d82
SuperDasher <manupicc02@gmail.com>
2022-10-03 18:44:44 +0200

    finish quicksort script

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -8,27 +8,36 @@ int main(int argc, char *argv[])
     argv++;
     argc--;
 
-    printf("array di parametri prima del sort:\n[");
-    for (int i = 0; i < argc - 1; i++)
+    if (argc < 2)
     {
-        argv[i] = atoi(argv[i]);
-        printf("%d, ", argv[i]);
+        printf("usage: %s <number> <number> <number> ...\n", argv[-1]);
+        return 1;
     }
-    printf("%d]\n", argv[argc - 1]);
 
-    qsort((void *)argv, (size_t)argc, sizeof(int *), compare);
-    
-    printf("\narray di parametri dopo del sort:\n");
+    int *numbers = malloc(argc * sizeof(int));
+    printf("array di parametri prima del sort:\n{");
     for (int i = 0; i < argc - 1; i++)
     {
-        printf("%d, ", argv[i]);
+        numbers[i] = atoi(argv[i]);
+        printf("%d, ", numbers[i]);
     }
-    printf("%d]\n", argv[argc - 1]);
+    numbers[argc - 1] = atoi(argv[argc - 1]);
+    printf("%d}\n", numbers[argc - 1]);
+
+    qsort((void *)numbers, (size_t)argc, sizeof(int), compare);
+
+    printf("\narray di parametri dopo il sort:\n{");
+    for (int i = 0; i < argc - 1; i++)
+    {
+        printf("%d, ", numbers[i]);
+    }
+    printf("%d}\n", numbers[argc - 1]);
+    free(numbers);
 
     return 0;
 }
 
-int compare(const void * arg1, const void * arg2)
+int compare(const void *arg1, const void *arg2)
 {
-    return (*(int *)arg1 - *(int *)arg2);
+    return *(int *)arg1 - *(int *)arg2;
 }
