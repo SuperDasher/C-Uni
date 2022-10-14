3f0b59906ac3d5ce1fb630ef7344412f69c7f68b
SuperDasher_mobile <manupicc02@gmail.com>
2022-10-14 12:55:06 +0000

    start out expenses script

--- src/manage_expenses.c
+++ src/manage_expenses.c
@@ -1,7 +1,20 @@
-// TODO: create code to manage the expenses (at least from command line)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MIN_CHARS_BACK 351
+#define STEP_BACK 3
+
 int main(int argc, char *argv[])
 {
+	FILE *fptr = fopen("money.md", "a+");
+	fseek(fptr, -MIN_CHARS_BACK, SEEK_END);
+	while (fgetc(fptr) != '#')
+	{
+		fseek(fptr, -STEP_BACK, SEEK_CUR);
+	}
 	
-
+	fclose(fptr);
 	return 0;
 }
