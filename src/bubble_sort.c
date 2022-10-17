dcb7496913095db7f483b00e3ee860026ae71b59
SuperDasher_mobile <manupicc02@gmail.com>
2022-10-17 12:28:08 +0000

    bubble sort

---
+++ src/bubble_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "../headers/array.h"
+
+#define N 10
+
+void bubble_sort(int *, int);
+void swap(int *, int *);
+
+int main()
+{
+	int array[N];
+
+	scan_a(array, N, "Inserisci i valori dell'array:\n");
+
+	printf("array prima:\n");
+	print_a(array, N);
+
+	bubble_sort(array, N);
+
+	printf("\narray dopo:\n");
+	print_a(array, N);
+	
+	return 0;
+}
+
+void bubble_sort(int *array, int length)
+{
+	for (int len = length - 1; len > 0; len--)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(&array[i], &array[i + 1]);
+			}
+		}
+	}
+}
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
