d8eaef144c01a1e06a42f9e0bd299b89e1905dca
SuperDasher <manupicc02@gmail.com>
2022-10-24 03:51:16 +0200

    start matrix move script

---
+++ src/matrix_move.c
@@ -0,0 +1,26 @@
+/* TODO: reminder: finish all the assignments on WeBeep
+	remaining ones: 4 - 5 - 6 (this one) - 7
+*/
+#include <stdio.h>
+
+#define N 4
+#define K 7
+
+enum move_flag
+{
+	BEHIND = -1,
+	NONE,
+	FRONT
+};
+struct move
+{
+	enum move_flag row;
+	enum move_flag col;
+};
+
+int main()
+{
+	struct move a[K];
+	// TODO: finish this
+	return 0;
+}
