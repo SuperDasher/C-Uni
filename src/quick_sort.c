57f98da513e1b98383cd4b53211429b9460f369e
SuperDasher <manupicc02@gmail.com>
2022-10-25 21:47:18 +0200

    fix argument count to exit and print to stderr

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 	{
-		printf("usage: %s <number> <number> <number> ...\n", argv[-1]);
-		return 1;
+		fprintf(stderr, "usage: %s <number> <number> <number> ...\n", argv[-1]);
+		exit(EXIT_FAILURE);
 	}
 
 	int *numbers = malloc(argc * sizeof(int));
