467dde1887071a1bf52b339c77db8b93b68c7444
SuperDasher <manupicc02@gmail.com>
2022-10-29 04:18:33 +0200

    make substitute substring script

--- src/substitute_substring.c
+++ src/substitute_substring.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define STR_LEN 101
 
 int main()
 {
-	// TODO: start coding
+	char string[STR_LEN];
+	char substring[STR_LEN];
+
+	printf("Inserisci la stringa:\n");
+	fgets(string, STR_LEN, stdin);
+	printf("Inserisci la sottostringa da sostituire:\n");
+	fgets(substring, STR_LEN, stdin);
+
+	string[strlen(string) - 1] = '\0';
+	substring[strlen(substring) - 1] = '\0';
+
+	for (int i = 0; string[i]; i++)
+	{
+		if (string[i] == substring[0])
+		{
+			bool found = true;
+			for (int j = 0; substring[j] != '\0'; j++)
+			{
+				if (string[i + j] != substring[j])
+				{
+					found = false;
+					break;
+				}
+			}
+			if (found)
+			{
+				for (int j = 0; substring[j]; j++)
+				{
+					string[i + j] = '*';
+				}
+			}
+		}
+	}
+
+	printf("%s\n", string);
 
 	return 0;
 }
