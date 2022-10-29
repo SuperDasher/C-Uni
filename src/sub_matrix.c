b160327c67c4e08139851d5e64c25da9571f7c6d
SuperDasher <manupicc02@gmail.com>
2022-10-29 03:41:19 +0200

    empty sub-matrix script

--- src/sub_matrix.c
+++ src/sub_matrix.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <dmatrix.h>
+
+#define SUBMATRIX_SIZE 3
 
 int main()
 {
-	// TODO: start coding
+	int **matrix;
+	int rows, columns;
+	init_m(&matrix, &rows, &columns, "Inserisci le dimensioni della matrice", "Righe: ", "Colonne: ");
+	if (rows < SUBMATRIX_SIZE || columns < SUBMATRIX_SIZE)
+	{
+		fprintf(stderr, "La matrice deve avere almeno %d righe e %d colonne", SUBMATRIX_SIZE, SUBMATRIX_SIZE);
+		exit(EXIT_FAILURE);
+	}
+	scan_dm(matrix, rows, columns, "Riga %d:");
+
+	for (int i = 0; i < rows - SUBMATRIX_SIZE; i++)
+	{
+		for (int j = 0; j < columns - SUBMATRIX_SIZE; j++)
+		{
+			int **submatrix;
+			if (!malloc_m(&submatrix, SUBMATRIX_SIZE, SUBMATRIX_SIZE))
+			{
+				fprintf(stderr, "malloc_m() failed: %s\n", strerror(errno));
+				exit(errno);
+			}
+			for (int k = 0; k < SUBMATRIX_SIZE; k++)
+			{
+				for (int l = 0; l < SUBMATRIX_SIZE; l++)
+				{
+					submatrix[k][l] = matrix[i + k][j + l];
+				}
+			}
+			if (is_empty(submatrix, SUBMATRIX_SIZE, SUBMATRIX_SIZE))
+			{
+				printf("Sotto-matrice vuota trovata in posizione (%d, %d)\n", i + 1, j + 1);
+				return 0;
+			}
+		}
+	}
+	printf("Nessuna sotto-matrice vuota trovata\n");
 
 	return 0;
 }
