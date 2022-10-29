8c6b44e36bde9b710351df9024125cdb4672b924
SuperDasher <manupicc02@gmail.com>
2022-10-29 02:23:19 +0200

    make perimeter script

--- src/perimeter.c
+++ src/perimeter.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <errno.h>
+#include <array.h>
 
 int main()
 {
-	// TODO: start coding
+	struct point
+	{
+		float x;
+		float y;
+	};
+	int n;
+	struct point *polygon;
+
+	printf("Quanti lati ha il poligono? ");
+	scanf("%d", &n);
+	if (n < 3)
+	{
+		fprintf(stderr, "Il poligono deve avere almeno 3 lati\n");
+		exit(EXIT_FAILURE);
+	}
+	polygon = malloc(n * sizeof(struct point));
+	if (polygon == NULL)
+	{
+		fprintf(stderr, "malloc() failed: %s\n", strerror(errno));
+		exit(errno);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("Inserisci le coordinate del vertice %d: ", i + 1);
+		scanf("%f%f", &polygon[i].x, &polygon[i].y);
+	}
+
+	float perimeter = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int next_i = (i + 1) % n;
+		perimeter += sqrt(pow(polygon[i].x - polygon[next_i].x, 2) + pow(polygon[i].y - polygon[next_i].y, 2));
+	}
+
+	printf("Il perimetro del poligono e' %f\n", perimeter);
 
 	return 0;
 }
