52e1cb80291f7c2fc20aecb43d5d53f610f6972f
SuperDasher <manupicc02@gmail.com>
2022-11-08 00:39:28 +0100

    use I flag instead of adding C_INCLUDE_PATH

--- compile.sh
+++ compile.sh
@@ -13,23 +13,11 @@ else
 	mkdir out
 fi
 
-#set the c/c++ include path to the directory headers/ if it's not already set
-if [ -z "$C_INCLUDE_PATH" ]; then
-	export C_INCLUDE_PATH="headers/"
-else
-	export C_INCLUDE_PATH=$C_INCLUDE_PATH:"headers/"
-fi
-if [ -z "$CPLUS_INCLUDE_PATH" ]; then
-	export CPLUS_INCLUDE_PATH="headers/"
-else
-	export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:"headers/"
-fi
-
 #compile every .c file in src as .o extension and every .cpp file as .opp extension and put them in out
 for file in src/*; do
 	if [[ "${file##*.}" == c ]]; then
-		gcc -Wall -Werror --pedantic -fdiagnostics-color=always -g -O0 "$file" -o "out/$(basename -- "$file" .c).o"
+		gcc -Iheaders/ -Wall -Werror --pedantic -fdiagnostics-color=always -g -O0 "$file" -o "out/$(basename -- "$file" .c).o"
 	elif [[ "${file##*.}" == cpp ]]; then
-		g++ -std=c++2a -Wall -Werror --pedantic -fdiagnostics-color=always -g -O0 "$file" -o "out/$(basename -- "$file" .cpp).opp"
+		g++ -Iheaders/ -std=c++2a -Wall -Werror --pedantic -fdiagnostics-color=always -g -O0 "$file" -o "out/$(basename -- "$file" .cpp).opp"
 	fi
 done
