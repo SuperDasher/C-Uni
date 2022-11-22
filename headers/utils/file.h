2e4de4130bcba640d885fe99b6a6d3de01bd0019
SuperDasher <manupicc02@gmail.com>
2022-11-22 15:53:24 +0100

    create file header

---
+++ headers/utils/file.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+long file_size(FILE *file)
+{
+	long current_position = ftell(file);
+	fseek(file, 0, SEEK_END);
+	long size = ftell(file);
+	fseek(file, current_position, SEEK_SET);
+	return size;
+}
