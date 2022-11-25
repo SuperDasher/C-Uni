eab6c20cda9ba7b3e1aac056a9496d0e4280a670
SuperDasher <manupicc02@gmail.com>
2022-11-25 01:45:45 +0100

    add doxygen comments to array header

--- headers/datastruct/array.h
+++ headers/datastruct/array.h
@@ -7,6 +7,13 @@
 #include <stdbool.h>
 #include <errno.h>
 
+/**
+ * @brief initialises a dynamic int array with a given size 
+ * 
+ * @param array 
+ * @param size 
+ * @param prompt asks for the size of the array 
+ */
 void init_a(int **array, int *size, char *prompt)
 {
 	printf("%s", prompt);
@@ -19,6 +26,14 @@ void init_a(int **array, int *size, char *prompt)
 	}
 }
 
+/**
+ * @brief asks to fill the int elements of an array 
+ * 
+ * @param array 
+ * @param dim 
+ * @param prompt can include "%d" to display the number of elements 
+ * - adds a newline after 
+ */
 void scan_a(int *array, int dim, char *prompt)
 {
 	char *prompt_newline = (char *)malloc(strlen(prompt) + 2);
@@ -40,6 +55,12 @@ void scan_a(int *array, int dim, char *prompt)
 	free(prompt_newline);
 }
 
+/**
+ * @brief prints an int array 
+ * 
+ * @param array 
+ * @param dim 
+ */
 void print_a(int *array, int dim)
 {
 	if (dim < 2)
@@ -55,6 +76,12 @@ void print_a(int *array, int dim)
 	printf("%d]\n", array[dim - 1]);
 }
 
+/**
+ * @brief assigns zero to every element in an int array 
+ * 
+ * @param array 
+ * @param dim 
+ */
 void empty_a(int *array, int dim)
 {
 	for (int i = 0; i < dim; i++)
@@ -63,6 +90,14 @@ void empty_a(int *array, int dim)
 	}
 }
 
+/**
+ * @brief checks if every element in an int array is equal to zero 
+ * 
+ * @param array 
+ * @param dim 
+ * @return true if every element in an int array is equal to zero. 
+ * @return false if at least one element in an int array is not equal to zero 
+ */
 bool is_empty_a(int *array, int dim)
 {
 	for (int i = 0; i < dim; i++)
@@ -75,6 +110,13 @@ bool is_empty_a(int *array, int dim)
 	return true;
 }
 
+/**
+ * @brief assigns the given value to every element in an int array 
+ * 
+ * @param array 
+ * @param dim 
+ * @param value int number to fill the array with 
+ */
 void fill_a(int *array, int dim, int value)
 {
 	for (int i = 0; i < dim; i++)
@@ -83,6 +125,14 @@ void fill_a(int *array, int dim, int value)
 	}
 }
 
+/**
+ * @brief fill an int array with random values going from min to max 
+ * 
+ * @param array 
+ * @param dim 
+ * @param min the minimum randomly generated value that can be assigned to an element of an int array 
+ * @param max the maximum randomly generated value that can be assigned to an element of an int array 
+ */
 void randfill_a(int *array, int dim, int min, int max)
 {
 	for (int i = 0; i < dim; i++)
@@ -91,6 +141,12 @@ void randfill_a(int *array, int dim, int min, int max)
 	}
 }
 
+/**
+ * @brief fills an array with the first natural numbers up to the array size 
+ * 
+ * @param array 
+ * @param dim 
+ */
 void succfill_a(int *array, int dim)
 {
 	for (int i = 0; i < dim; i++)
