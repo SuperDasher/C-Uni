fdb3fb423309ea5948aacb07f5df449ca572aa7c
SuperDasher <manupicc02@gmail.com>
2022-12-08 22:33:38 +0100

    add debug info with g3 option

--- compile.sh
+++ compile.sh
@@ -442,10 +442,10 @@ main() {
 		local -- fail_message
 		local -- success
 		if [[ "$file" == *.c ]]; then
-			fail_message=$(gcc -Iheaders/ ${c_version:+-std=c${c_version[*]}} -x c -Wall -Werror --pedantic -fdiagnostics-color=always -g -O0 "$file" "${definition_c_files[@]}" -o "out/${file:4:-2}.o" 2>&1)
+			fail_message=$(gcc -Iheaders/ ${c_version:+-std=c${c_version[*]}} -x c -Wall -Werror --pedantic -fdiagnostics-color=always -g3 -O0 "$file" "${definition_c_files[@]}" -o "out/${file:4:-2}.o" 2>&1)
 			success=$?
 		elif [[ "$file" == *.cpp ]]; then
-			fail_message=$(g++ -Iheaders/ ${cpp_version:+-std=c++${cpp_version[*]}} -x c++ -Wall -Werror --pedantic -fdiagnostics-color=always -g -O0 "$file" "${definition_cpp_files[@]}" -o "out/${file:4:-4}.opp" 2>&1)
+			fail_message=$(g++ -Iheaders/ ${cpp_version:+-std=c++${cpp_version[*]}} -x c++ -Wall -Werror --pedantic -fdiagnostics-color=always -g3 -O0 "$file" "${definition_cpp_files[@]}" -o "out/${file:4:-4}.opp" 2>&1)
 			success=$?
 		fi
 		((current_file++))
