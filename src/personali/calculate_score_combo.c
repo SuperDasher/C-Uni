a7854f559213bf07f404d8badb935aea9db8303b
SuperDasher <manupicc02@gmail.com>
2023-05-06 18:58:24 +0200

    combo calculator doesn't stop until 0 is typed

--- src/personali/calculate_score_combo.c
+++ src/personali/calculate_score_combo.c
@@ -1,5 +1,5 @@
 // create a program that calculates the resulting score you get depending from the combo knowing that the formula for it in math terms is:
-//score =  sum of i = 1 to combo(10 + floor(i * .5))
+// score =  sum of i = 1 to combo(10 + floor(i * .5))
 // example: if combo is 6 (considering that floor rounds up down to the nearest integer down)
 // score = 10 + 11 + 11 + 12 + 12 + 13 = 69
 
@@ -8,14 +8,16 @@
 
 int main(void)
 {
-	int combo;
-	int score = 0;
-	int i;
-	printf("Enter combo: ");
-	scanf("%d", &combo);
-	for (i = 1; i <= combo; i++)
+	int combo = 1;
+	do
 	{
-		score += 10 + floor(i * .5);
-	}
-	printf("Score: %d\n", score);
+		int score = 0;
+		printf("Enter combo: ");
+		scanf("%d", &combo);
+		for (int i = 1; i <= combo; i++)
+		{
+			score += 10 + floor(i * .5);
+		}
+		printf("Score: %d\n", score);
+	} while (combo != 0);
 }
